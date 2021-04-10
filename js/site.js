22f7c82521830ff0a2e9bf36c49ede484aed44c5
Suvash Shah Thakuri <suvash@appmozart.com>
2021-04-09 18:54:29 -0700

    player's activity div added

--- js/site.js
+++ js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 let currentTurn = "O";
 //count boxes marked
-let counter = 0;
+let counter = 1;
 let setTimer;
 let elapsed_seconds = 0;
 
@@ -12,9 +12,22 @@ startTimer();
 $("#lblText").text(currentTurn + "'s turn");
 $("#lblNew").hide();
 
-$("[id^='btn']").click(function () {
+$("[id^='btn']").on("click", function () {
   $(this).text(currentTurn);
   $(this).attr("disabled", "disabled");
+  //display activity
+  $("#activityList").append(
+    '<li class="list-group-item small">' +
+      counter +
+      ". Player " +
+      currentTurn +
+      " marked (" +
+      $(this).attr("id").split("-")[1] +
+      "," +
+      $(this).attr("id").split("-")[2] +
+      ")</li>"
+  );
+
   if (currentTurn === "O") {
     currentTurn = "X";
   } else {
@@ -23,6 +36,10 @@ $("[id^='btn']").click(function () {
   counter++;
   $("#lblText").text(currentTurn + "'s turn");
   isOver();
+
+  //scroll to bottom of div
+  var objDiv = document.getElementById("activityList");
+  objDiv.scrollTop = objDiv.scrollHeight;
 });
 
 function isOver() {
@@ -31,56 +48,58 @@ function isOver() {
   let winner = [];
   // eight possible cases
   // 3 horizontals, 3 verticals, 2 diagonals
+  //horizontals
   let c1 =
-    $("#btn-a1").text() === $("#btn-a2").text() &&
-    $("#btn-a2").text() === $("#btn-a3").text() &&
-    !!$("#btn-a1").text();
-  winner.push(c1, $("#btn-a1").text());
+    $("#btn-1-1").text() === $("#btn-1-2").text() &&
+    $("#btn-1-2").text() === $("#btn-1-3").text() &&
+    !!$("#btn-1-1").text();
+  winner.push(c1, $("#btn-1-1").text());
 
   let c2 =
-    $("#btn-b1").text() === $("#btn-b2").text() &&
-    $("#btn-b2").text() === $("#btn-b3").text() &&
-    !!$("#btn-b1").text();
-  winner.push(c2, $("#btn-b1").text());
+    $("#btn-2-1").text() === $("#btn-2-2").text() &&
+    $("#btn-2-2").text() === $("#btn-2-3").text() &&
+    !!$("#btn-2-1").text();
+  winner.push(c2, $("#btn-2-1").text());
   let c3 =
-    $("#btn-c1").text() === $("#btn-c2").text() &&
-    $("#btn-c2").text() === $("#btn-c3").text() &&
-    !!$("#btn-c1").text();
-  winner.push(c3, $("#btn-c1").text());
+    $("#btn-3-1").text() === $("#btn-3-2").text() &&
+    $("#btn-3-2").text() === $("#btn-3-3").text() &&
+    !!$("#btn-3-1").text();
+  winner.push(c3, $("#btn-3-1").text());
+  //vertical
   let c4 =
-    $("#btn-a1").text() === $("#btn-b1").text() &&
-    $("#btn-b1").text() === $("#btn-c1").text() &&
-    !!$("#btn-a1").text();
-  winner.push(c4, $("#btn-a1").text());
+    $("#btn-1-1").text() === $("#btn-2-1").text() &&
+    $("#btn-2-1").text() === $("#btn-3-1").text() &&
+    !!$("#btn-1-1").text();
+  winner.push(c4, $("#btn-1-1").text());
   let c5 =
-    $("#btn-a2").text() === $("#btn-b2").text() &&
-    $("#btn-b2").text() === $("#btn-c2").text() &&
-    !!$("#btn-a2").text();
-  winner.push(c5, $("#btn-a2").text());
+    $("#btn-1-2").text() === $("#btn-2-2").text() &&
+    $("#btn-2-2").text() === $("#btn-3-2").text() &&
+    !!$("#btn-1-2").text();
+  winner.push(c5, $("#btn-1-2").text());
   let c6 =
-    $("#btn-a3").text() === $("#btn-b3").text() &&
-    $("#btn-b3").text() === $("#btn-c3").text() &&
-    !!$("#btn-a3").text();
-  winner.push(c6, $("#btn-a3").text());
+    $("#btn-1-3").text() === $("#btn-2-3").text() &&
+    $("#btn-2-3").text() === $("#btn-3-3").text() &&
+    !!$("#btn-1-3").text();
+  winner.push(c6, $("#btn-1-3").text());
   //diagonal
   let c7 =
-    $("#btn-a1").text() === $("#btn-b2").text() &&
-    $("#btn-b2").text() === $("#btn-c3").text() &&
-    !!$("#btn-a1").text();
-  winner.push(c7, $("#btn-a1").text());
+    $("#btn-1-1").text() === $("#btn-2-2").text() &&
+    $("#btn-2-2").text() === $("#btn-3-3").text() &&
+    !!$("#btn-1-1").text();
+  winner.push(c7, $("#btn-1-1").text());
   let c8 =
-    $("#btn-a3").text() === $("#btn-b2").text() &&
-    $("#btn-b2").text() === $("#btn-c1").text() &&
-    !!$("#btn-a3").text();
-  winner.push(c8, $("#btn-a3").text());
+    $("#btn-1-3").text() === $("#btn-2-2").text() &&
+    $("#btn-2-2").text() === $("#btn-3-1").text() &&
+    !!$("#btn-1-3").text();
+  winner.push(c8, $("#btn-1-3").text());
   console.log(winner);
   chk = jQuery.inArray(true, winner);
   console.log(chk);
-  if (chk != -1 || counter == 9) {
+  if (chk != -1 || counter > 9) {
     // if so, show the result,
     if (chk > -1) {
       //there is awinner
-      $("#lblText").text("winner is " + winner[chk + 1]);
+      $("#lblText").text("Winner is " + winner[chk + 1]);
     } else {
       // there is no winner
       $("#alert").addClass("row alert alert-warning");
@@ -94,6 +113,11 @@ function isOver() {
     //stop the timer
     clearInterval(setTimer);
     $("#lbltime").hide();
+
+    //display activity
+    $("#activityList").append(
+      '<li class="list-group-item small">Winner is ' + winner[chk + 1] + "</li>"
+    );
   } else {
     //if not, re-start the timer
     elapsed_seconds = 0;
